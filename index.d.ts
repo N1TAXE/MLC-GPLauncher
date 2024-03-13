6b3a9e2cfa597bae9338bf6fa2fc6eae1986d622
N1TAXE <n1t4x3@gmail.com>
2024-03-13 17:04:00 +0100

    Update

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,32 @@
 declare module "gpl-core" {
   type OS = "windows" | "osx" | "linux";
 
+  interface ModTypes {
+    slug: string;
+    title: string;
+    description: string;
+    categories: string[];
+    client_side: string;
+    server_side: string;
+    project_type: string;
+    downloads: number;
+    icon_url: string;
+    color: number;
+    thread_id: string;
+    monetization_status: string;
+    project_id: string;
+    author: string;
+    display_categories: string[];
+    versions: string[];
+    follows: number;
+    date_created: string;
+    date_modified: string;
+    latest_version: string;
+    license: string;
+    gallery: string[];
+    featured_gallery: string;
+  }
+
   interface IOverrides {
     /**
      * The amount of launch arguments specified in the version file before it adds the default again
